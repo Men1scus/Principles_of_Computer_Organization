bb104b98d9b257040a7f37200cbd2ac51e44bd53
Men1scus <zzh_cd@126.com>
2024-03-28 20:15:53 +0800

    ✨ feat: 2_multiply

--- Experiment/2_multiply/multiply.v
+++ Experiment/2_multiply/multiply.v
@@ -40,27 +40,55 @@ module multiply(              // 乘法器
   	assign op1_absolute = op1_sign ? (~mult_op1+1) : mult_op1; // 是1就按位取反
     assign op2_absolute = op2_sign ? (~mult_op2+1) : mult_op2;
 
-    //加载被乘数，运算时每次左移一位
+    // //加载被乘数，运算时每次左移一位
+    // reg  [63:0] multiplicand;
+    // always @ (posedge clk)
+    // begin
+    //     if (mult_valid)
+    //     begin    // 如果正在进行乘法，则被乘数每时钟左移一位
+    //         multiplicand <= {multiplicand[62:0],1'b0}; // 拼接，补零
+    //     end
+    //     else if (mult_begin) 
+    //     begin   // 乘法开始，加载被乘数，为乘数1的绝对值
+    //         multiplicand <= {32'd0,op1_absolute};
+    //     end
+    // end
+
+     //加载被乘数，运算时每次左移2位
     reg  [63:0] multiplicand;
     always @ (posedge clk)
     begin
         if (mult_valid)
-        begin    // 如果正在进行乘法，则被乘数每时钟左移一位
-            multiplicand <= {multiplicand[62:0],1'b0}; // 拼接，补零
+        begin    // 如果正在进行乘法，则被乘数每时钟左移2位
+            multiplicand <= {multiplicand[61:0], 2'b00}; // 拼接，补零
         end
         else if (mult_begin) 
         begin   // 乘法开始，加载被乘数，为乘数1的绝对值
-            multiplicand <= {32'd0,op1_absolute};
+            multiplicand <= {32'd0, op1_absolute};
         end
     end
 
-    //加载乘数，运算时每次右移一位
+    // //加载乘数，运算时每次右移一位
+    // reg  [31:0] multiplier;
+    // always @ (posedge clk)
+    // begin
+    //     if (mult_valid)
+    //     begin   // 如果正在进行乘法，则乘数每时钟右移一位
+    //         multiplier <= {1'b0,multiplier[31:1]}; 
+    //     end
+    //     else if (mult_begin)
+    //     begin   // 乘法开始，加载乘数，为乘数2的绝对值
+    //         multiplier <= op2_absolute; 
+    //     end
+    // end
+
+    //加载乘数，运算时每次右移2位
     reg  [31:0] multiplier;
     always @ (posedge clk)
     begin
         if (mult_valid)
-        begin   // 如果正在进行乘法，则乘数每时钟右移一位
-            multiplier <= {1'b0,multiplier[31:1]}; 
+        begin   // 如果正在进行乘法，则乘数每时钟右移2位
+            multiplier <= {2'b00,multiplier[31:2]}; 
         end
         else if (mult_begin)
         begin   // 乘法开始，加载乘数，为乘数2的绝对值
@@ -70,15 +98,19 @@ module multiply(              // 乘法器
     
     // 部分积：乘数末位为1，由被乘数左移得到；乘数末位为0，部分积为0
     wire [63:0] partial_product;
+    wire [63:0] partial_product2;
+
     assign partial_product = multiplier[0] ? multiplicand : 64'd0;
-    
+    assign partial_product2 = multiplier[1] ? (multiplicand << 1) : 64'd0;
+
     //累加器
     reg [63:0] product_temp;
     always @ (posedge clk)
     begin
         if (mult_valid)
         begin
-            product_temp <= product_temp + partial_product;
+            // product_temp <= product_temp + partial_product;
+            product_temp <= product_temp + partial_product + partial_product2;
         end
         else if (mult_begin) 
         begin
--- Experiment/2_multiply/tb.v
+++ Experiment/2_multiply/tb.v
@@ -1,27 +1,3 @@
-`timescale 1ns / 1ps
-
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   17:13:38 04/15/2016
-// Design Name:   multiply
-// Module Name:   F:/new_lab/multiply/tb.v
-// Project Name:  multiply
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: multiply
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module tb;
 
     // Inputs
@@ -34,7 +10,7 @@ module tb;
     wire [63:0] product;
     wire mult_end;
 
-    // Instantiate the Unit Under Test (UUT)
+    // Instantiate the UUT
     multiply uut (
         .clk(clk), 
         .mult_begin(mult_begin), 
@@ -53,30 +29,37 @@ module tb;
 
         // Wait 100 ns for global reset to finish
         #100;
-        mult_begin = 1;
-        mult_op1 = 32'H00001111;
-        mult_op2 = 32'H00001111; //result: 1234321
-        #400;
-        mult_begin = 0;
-        #500;
-        mult_begin = 1;
-        mult_op1 = 32'H00001111;
-        mult_op2 = 32'H00002222;
-        #400;
-        mult_begin = 0;
-        #500;
-        mult_begin = 1;
-        mult_op1 = 32'H00000002;
-        mult_op2 = 32'HFFFFFFFF;
-        #400;
-        mult_begin = 0;
-        #500;
-        mult_begin = 1;
-        mult_op1 = 32'H00000002;
-        mult_op2 = 32'H80000000;
-        #400;
-        mult_begin = 0;
-        // Add stimulus here
+        
+        perform_multiplication(32'H00001111, 32'H00001111); // Expected result: 1234321
+        perform_multiplication(32'H00001111, 32'H00002222); // Example test case
+        perform_multiplication(32'H00000002, 32'HFFFFFFFF); // Edge case: large negative number
+        perform_multiplication(32'H00000002, 32'H80000000); // Edge case: overflow
+        perform_multiplication(32'H00000000, 32'HFFFFFFFF); // Zero test case
+        perform_random_tests(10); // Perform 10 random tests
     end
-   always #5 clk = ~clk;
+
+    // Always toggle clock every 5ns
+    always #5 clk = ~clk;
+
+    task perform_multiplication(input [31:0] op1, input [31:0] op2);
+        begin
+            #100;
+            mult_begin = 1;
+            mult_op1 = op1;
+            mult_op2 = op2;
+            #400;
+            mult_begin = 0;
+            #500;
+        end
+    endtask
+
+    task perform_random_tests(input integer count);
+        integer i;
+        begin
+            for (i = 0; i < count; i = i + 1) begin
+                perform_multiplication($random, $random);
+            end
+        end
+    endtask
+
 endmodule
